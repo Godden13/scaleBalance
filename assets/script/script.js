c63521c591947d57719a5a6b1a9b5391e364f51d
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2022-12-15 15:30:18 +0100

    fix linters

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,7 +3,7 @@ const secondInput = document.getElementById('input2')
 const weightList = document.getElementById('list')
 const display = document.getElementById('display')
 const buttonElement = document.getElementById('balance')
-const arr = new Array()
+let arr = new Array()
 let list
 function scaling () {
   const a = firstInput.value
@@ -14,8 +14,8 @@ function scaling () {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if ((parseInt(a) + parseInt(arr[i])) === (parseInt(b) + parseInt(arr[j]))) {
-       return (`${arr[i]},${arr[j]}`)
-       } else {
+        return (`${arr[i]},${arr[j]}`)
+        } else {
         return ('Cannot balance')
       }
     }
