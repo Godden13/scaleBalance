312271ea92165260d7c7bf254a38429a4f267e48
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2022-12-15 14:56:40 +0100

    fix linters

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,22 @@
-let firstInput = document.getElementById('input1')
-let secondInput = document.getElementById('input2')
-let weightList = document.getElementById('list')
-let display = document.getElementById('display')
-let buttonElement = document.getElementById('balance')
-let arr = new Array()
-var list
-function scaling() {
-  let a = firstInput.value;
-  let b = secondInput.value;
+const firstInput = document.getElementById('input1')
+const secondInput = document.getElementById('input2')
+const weightList = document.getElementById('list')
+const display = document.getElementById('display')
+const buttonElement = document.getElementById('balance')
+const arr = new Array()
+let list
+function scaling () {
+  const a = firstInput.value
+  const b = secondInput.value
   list = weightList.value
   arr = list.split(',')
-  
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      if ((parseInt(a)+ parseInt(arr[i])) === (parseInt(b)+ parseInt(arr[j]))) {
+      if ((parseInt(a) + parseInt(arr[i])) === (parseInt(b) + parseInt(arr[j]))) {
        return (`${arr[i]},${arr[j]}`)
-      }else {
-        return (`Cannot balance`)
+       } else {
+        return ('Cannot balance')
       }
     }
   }
