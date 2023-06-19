25b9ad87f6642639f06cd7769fcfd76b1aae2349
Godden13 <chrissgodden@gmail.com>
2023-06-19 12:41:31 +0100

    return correct value

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,20 +3,18 @@ const secondInput = document.getElementById('input2')
 const weightList = document.getElementById('list')
 const display = document.getElementById('display')
 const buttonElement = document.getElementById('balance')
-let arr = []
 let list
+
 function scaling() {
-  const a = firstInput.value
-  const b = secondInput.value
+  const valueA = firstInput.value
+  const valueB = secondInput.value
   list = weightList.value
-  arr = list.split(',')
+  let arr = list.split(',')
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      if ((parseInt(a) + parseInt(arr[i])) === (parseInt(b) + parseInt(arr[j]))) {
+      if ((parseInt(valueA) + parseInt(arr[i])) === (parseInt(valueB) + parseInt(arr[j]))) {
         return (`${arr[i]},${arr[j]}`)
-      } else {
-        return ('Cannot balance')
       }
     }
   }
@@ -25,3 +23,4 @@ function scaling() {
 buttonElement.addEventListener('click', () => {
   display.innerHTML = scaling()
 })
+
