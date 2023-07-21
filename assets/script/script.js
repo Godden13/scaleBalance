07445eaf9726453d790efee6ad6094ccc9070818
Godden13 <chrissgodden@gmail.com>
2023-07-21 06:32:27 +0100

    fix linting

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,12 +3,12 @@ const secondInput = document.getElementById('input2')
 const weightList = document.getElementById('list')
 const display = document.getElementById('display')
 const buttonElement = document.getElementById('balance')
-let list;
+let list
 
 function scaling () {
-  const valueA = firstInput.value;
-  const valueB = secondInput.value;
-  list = weightList.value;
+  const valueA = firstInput.value
+  const valueB = secondInput.value
+  list = weightList.value
   const arr = list.trim().split(',')
 
   for (let i = 0; i < arr.length; i++) {
@@ -17,10 +17,10 @@ function scaling () {
         parseInt(valueA) + parseInt(arr[i]) ===
         parseInt(valueB) + parseInt(arr[j])
       ) {
-        return `${arr[i]},${arr[j]}`;
+        return `${arr[i]},${arr[j]}`
       }
     }
-  } return 'cannot balance';
+  } return 'cannot balance'
 }
 
 buttonElement.addEventListener('click', () => {
