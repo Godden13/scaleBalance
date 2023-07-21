f0d1a17c43141636d7486c81312577439bc3e3e5
Godden13 <chrissgodden@gmail.com>
2023-07-21 06:28:25 +0100

    fix linting

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,15 @@
-const firstInput = document.getElementById("input1");
-const secondInput = document.getElementById("input2");
-const weightList = document.getElementById("list");
-const display = document.getElementById("display");
-const buttonElement = document.getElementById("balance");
+const firstInput = document.getElementById('input1')
+const secondInput = document.getElementById('input2')
+const weightList = document.getElementById('list')
+const display = document.getElementById('display')
+const buttonElement = document.getElementById('balance')
 let list;
 
-function scaling() {
+function scaling () {
   const valueA = firstInput.value;
   const valueB = secondInput.value;
   list = weightList.value;
-  const arr = list.trim().split(",");
+  const arr = list.trim().split(',')
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
@@ -17,20 +17,12 @@ function scaling() {
         parseInt(valueA) + parseInt(arr[i]) ===
         parseInt(valueB) + parseInt(arr[j])
       ) {
-        console.log("Hello");
-
         return `${arr[i]},${arr[j]}`;
-      } 
+      }
     }
-  } {
-    console.log("Hi");
-
-    return "cannot balance";
-  }
-
+  } return 'cannot balance';
 }
 
-
-buttonElement.addEventListener("click", () => {
-  display.innerHTML = scaling();
-});
+buttonElement.addEventListener('click', () => {
+  display.innerHTML = scaling()
+})
